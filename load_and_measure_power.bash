b255af1daf947c1c73b5e1877866858fea0f026d
Erfan Sharafzadeh <e.sharafzadeh@outlook.com>
2018-05-14 13:39:33 -0400

    fixing the experiment script

--- load_and_measure_power.bash
+++ load_and_measure_power.bash
@@ -7,8 +7,8 @@ for i in `seq $2 $3 $4`
 do
     echo "experiment started with rate=$i"
     ssh root@10.254.254.100 '/home/erfan/rapl_read/reader.bash > test.log &'
-    temp=$(/home/erfan/Documents/programming/mutilate/mutilate -t 5 -q $i -s 110.254.254.100)
-    ssh root@10.254.254.100 '/home/erfan/rapl_read/reader.bash > test.log &'
+    temp=$(/home/scc/mutilate/mutilate -t 60 -c 25 -T 4 -D 4 -C 4 -q $i -s 10.254.254.100 -a 10.254.254.13 -a localhost)
+    ssh root@10.254.254.100 'pkill reader.bash'
     scp root@10.254.254.100:/root/test.log "rate$i.log"
     ssh root@10.254.254.100 'rm test.log'
     echo -n "$i," >> $1
