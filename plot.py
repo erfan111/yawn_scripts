70ba43ffce2608d65a1a91a24672a861c6f8ff7c
Erfan Sharafzadeh <e.sharafzadeh@outlook.com>
2018-07-22 19:04:19 +0430

    updating the plot files

--- plot.py
+++ plot.py
@@ -5,28 +5,28 @@ import sys
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
-a = np.genfromtxt(sys.argv[1], delimiter='J')
+a = np.genfromtxt(sys.argv[1], delimiter=',')
 print(a)
-b = np.genfromtxt(sys.argv[2], delimiter='J')
+b = np.genfromtxt(sys.argv[2], delimiter=',')
 print(b)
 
 x = [i[0] for i in a]
 on_avg = [i[1] for i in a]
 off_avg = [i[1] for i in b]
-on_95 = [i[2] for i in a]
-off_95 = [i[2] for i in b]
-on_99 = [i[3] for i in a]
-off_99 = [i[3] for i in b]
-on_999 = [i[4] for i in a]
-off_999 = [i[4] for i in b]
+on_99 = [i[2] for i in a]
+off_99 = [i[2] for i in b]
+on_999 = [i[3] for i in a]
+off_999 = [i[3] for i in b]
+on_9999 = [i[4] for i in a]
+off_9999 = [i[4] for i in b]
 plt.ylabel('Latency (us)')
 plt.title('Request Latency of different loads')
 # plt.xticks(ind, ('20%', '30%', '40%', '50%'))
 plt.yticks(np.arange(0, 800, 10))
 plt.grid()
-#p1,p2,p3,p4,p5,p6,p7,p8 = plt.plot(x, on_avg, 'r', x, off_avg, 'r:', x, on_95, 'y',x, off_95, 'y:', x, on_99, 'b',x, off_99, 'b:', x, on_999, 'g', x, off_999, 'g:')
-p1,p2 = plt.plot(x, on_avg, 'g', x, off_avg, 'g:')
-#plt.legend((p1, p2,p3,p4,p5,p6,p7,p8), ('On-Avg', 'Off-Avg' ,'On-95th', 'Off-95th','On-99th', 'Off-99th', 'On-99.9', 'Off-99.9'))
-plt.legend((p1, p2), ('On-avg', 'Off-avg'))
+p1,p2,p3,p4,p5,p6,p7, p8 = plt.plot(x, on_avg, 'r', x, off_avg, 'r:', x, on_99, 'b',x, off_99, 'b:', x, on_999, 'g', x, off_999, 'g:',x, on_9999, 'm', x, off_9999, 'm:')
+# p1,p2 = plt.plot(x, on_avg, 'g', x, off_avg, 'g:')
+plt.legend((p1, p2,p3,p4,p5,p6, p7,p8), ('menu-Avg', 'yawn-Avg','menu-99th', 'yawn-99th', 'menu-99.9', 'yawn-99.9', 'menu-99.99', 'yawn-99.99'))
+# plt.legend((p1, p2), ('On-avg', 'Off-avg'))
 #plt.savefig('avg.png', format='png', dpi=300)
 plt.show()
--- plot_joules_errorbar.py
+++ plot_joules_errorbar.py
@@ -6,14 +6,18 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from scipy import stats
 
-# convertfunc = lambda x: float(x[0:-2])
-# a = np.genfromtxt(sys.argv[1],  converters={0: convertfunc})
-# print(a)
-# if(len(sys.argv) >2):
-# 	b = np.genfromtxt(sys.argv[2],  converters={0: convertfunc})
-# 	print(b)
-#
-# x = range(len(a))
+a = list(np.genfromtxt(sys.argv[1], delimiter=','))
+print(a)
+
+x = []
+if isinstance(a[0], list):
+	for i in range(len(a)):
+		x.append(int(a[i][0]))
+		np.delete(a[i],0)
+else:
+	for i in range(len(a)):
+		x.append(int(a[0]))
+		np.delete(a,0)
 
 data = [
 [13,11.5,12.2,11,11,11.2],
@@ -24,15 +28,25 @@ data = [
 [13,11.5,12.2,11,11,11.2]
 ]
 
-x = range(len(data))
-print(stats.sem(data[0]))
+# treatments = [e1, e2, e3, e4]
+# med1, CI1 = fakeBootStrapper(1)
+# med2, CI2 = fakeBootStrapper(2)
+# medians = [None, None, med1, med2]
+# conf_intervals = [None, None, CI1, CI2]
+
+pos = np.array(range(len(a))) + 1
+
 
 fig, axs = plt.subplots()
-axs.boxplot(data, 0, '')
+# axs.boxplot(a, sym='k+', positions=pos,
+#                 notch=1, bootstrap=5000)
+axs.boxplot(a, 0, '')
 # axs.set_title("don't show\noutlier points")
 plt.ylabel('Energy (J)')
 plt.title('Power Consumption of Memcached in different Loads')
-# plt.xticks(range(1000,51000,2000))
+axs.set_xticklabels(x, rotation='90')
+plt.grid(axis='both', which='both')
+# plt.xticks(x)
 # #plt.yticks(np.arange(0, 800, 10))
 # plt.grid()
 # #p1,p2,p3,p4,p5,p6,p7,p8 = plt.plot(x, on_avg, 'r', x, off_avg, 'r:', x, on_95, 'y',x, off_95, 'y:', x, on_99, 'b',x, off_99, 'b:', x, on_999, 'g', x, off_999, 'g:')
