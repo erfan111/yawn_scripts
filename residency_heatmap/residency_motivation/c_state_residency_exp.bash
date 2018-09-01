577ec8ecc228a6827c5d9ea09bf3617b6d1fe7df
Erfan SHarafzadeh <e.sharafzadeh@outlook.com>
2018-09-01 17:41:09 +0430

    recent version of traec scripts

--- residency_heatmap/residency_motivation/c_state_residency_exp.bash
+++ residency_heatmap/residency_motivation/c_state_residency_exp.bash
@@ -5,7 +5,7 @@ for i in `seq $1 $2 $3`
 do
     echo "experiment started with rate=$i"
     ssh root@10.254.254.100 '/home/erfan/rapl_read/read_residencies.bash > test.log &'
-    temp=$(/home/erfan/mutilate/mutilate -t 60 -c 25 -T 25 -D 4 -C 4 -q $i -s 10.254.254.100 -i fb_ia)
+    temp=$(/home/erfan/mutilate/mutilate -t 60 -c 25 -T 25 -q $i -w 20 -s 10.254.254.100 -i fb_ia)
     scp root@10.254.254.100:/root/test.log "rate$i.log"
     ssh root@10.254.254.100 'rm test.log'
     sleep 15
